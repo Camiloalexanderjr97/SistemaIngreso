e28ba2020103c40f73d15b7d4ff5992e400d33b8
Duvanlabrador27 <carlosduvanlh@ufps.edu.co>
2022-05-10 12:26:08 -0500

    DataBaseCredenciales

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'recepcion'
+        host: 'qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'fpl74w122m438iah',
+        password: '	iv37dx5s6pegrhnv',
+        database: 'oalbgxyef2reg40p'
     }
 }
