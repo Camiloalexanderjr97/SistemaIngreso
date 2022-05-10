b6dd7b8d26320ee6e053fabbf50bd91ef7b850e7
Duvanlabrador27 <carlosduvanlh@ufps.edu.co>
2022-05-10 12:31:57 -0500

    correcionData

--- src/keys.js
+++ src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         host: 'qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         user: 'fpl74w122m438iah',
-        password: '	iv37dx5s6pegrhnv',
+        password: 'iv37dx5s6pegrhnv',
         database: 'oalbgxyef2reg40p'
     }
 }
